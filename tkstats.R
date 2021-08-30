6f62b119703acc8855bc1c2bc80be04619ebe36e
Wambaugh <Wambaugh.John@epa.gov>
2021-08-30 12:30:44 -0400

    Fixing 2compartment and opera

--- tkstats.R
+++ tkstats.R
@@ -74,8 +74,14 @@ makeCvTpreds <- function(CvT.data,label)
             }
             Cmax.obs <- max(this.subset4means$Value,na.rm=T)
             Cmax.pred <- max(pred[,"Cven"],na.rm=T)
-            AUC.obs <- AUC(this.subset4means$Time,this.subset4means$Value)
-            AUC.pred <- AUC(pred[,"time"],pred[,"Cven"])
+            if (length(unique(this.subset4means$Time))>1)
+            {
+              AUC.obs <- AUC(this.subset4means$Time,this.subset4means$Value)
+              AUC.pred <- AUC(pred[,"time"],pred[,"Cven"])
+            } else {
+              AUC.obs <- NA
+              AUC.pred <- NA
+            }
             new.row <- data.frame(
               Compound=this.compound,
               DTXSID=this.dtxsid,
@@ -101,43 +107,257 @@ makeCvTpreds <- function(CvT.data,label)
     stats=stats.table))
 }
 
+#'Analytical 1-compartment model
+#'
+#'@param time A vector of times in hours
+#'@param params A named list of model parameter values. Must include:
+#'\describe{
+#'\item{kelim}{Elimination rate, 1/h}
+#'\item{Vdist}{Volume of distribution, L/kg body weight}}
+#'For oral administration (\code{iv.dose} FALSE), \code{params} must also include:
+#'\describe{
+#'\item{Fgutabs}{Oral bioavailability, unitless fraction}
+#'\item{kgutabs}{Oral absorption rate, 1/h}}
+#'@param dose Dose in mg/kg
+#'@param iv.dose TRUE for single IV bolus dose; FALSE for single oral dose
+#'
+#'@return A vector of plasma concentration values corresponding to \code{time}.
+#'
+#'@author Caroline Ring, John Wambaugh
+#'
+#' @export cp_1comp
+cp_1comp <- function(time, params, dose, iv.dose){
+  #time in hours
+  #dose in mg/kg
+  #params: a subset of those returned by httk::parameterize_1comp
+  #a named list
+  #Fgutabs = oral fraction absorbed, unitless
+  #kelim = elimination rate, 1/h
+  #kgutabs = oral absorption rate, 1/h
+  #Vdist = volume of distribution, L/kg body weight
+  if (any(sapply(params,function(x) identical(x,numeric(0))))) return(0)
+  
+  if (is.null(params$Fgutabs)|is.na(params$Fgutabs)) 
+  {
+    params$Fgutabs<-1
+  }
+  if (is.null(params$kgutabs)|is.na(params$kgutabs)) 
+  {
+    params$kgutabs<-1
+  }  
+  if (params$Fgutabs>1) params$Fgutabs<-1
+  
+  if (iv.dose){
+    cp <- dose*exp(-params$kelim * time)/params$Vdist
+  }else{
+    
+    cp <- (params$Fgutabs * dose *
+             params$kgutabs*(exp(-params$kelim * time) - exp(-params$kgutabs* time)))/
+      (params$Vdist*(params$kgutabs - params$kelim))
+    #Note: this fails if kgutabs == kelim,
+    #which usually happens if both are at the upper bound
+  }
+  cp[cp<0] <- 0
+  cp[!is.finite(cp)] <- 0
+  return(cp)
+}
+
+#' Analytical 2-compartment model
+#' 
+#' 
+#' @param params A named list of parameter values including the following:
+#' \describe{ \item{k12}{Rate at which compound moves from central to
+#' peripheral compartment} \item{k21}{Rate at which compound moves from
+#' peripheral to central compartment} \item{kelim}{Elimination rate}
+#' \item{V1}{Apparent volume of central compartment} } For oral administration
+#' (\code{iv.dose} FALSE), \code{params} must also include: \describe{
+#' \item{Fgutabs}{Oral bioavailability} \item{kgutabs}{Rate of absorption from
+#' gut} }
+#' 
+#' !author Caroline Ring, John Wambaugh
+#' @param time A vector of time values, in hours
+#' @param dose A dose in mg/kg
+#' @param iv.dose TRUE for single IV bolus dose, FALSE for single oral dose
+#' @return A vector of plasma concentration values corresponding to each value
+#' in \code{time}
+#' @export cp_2comp
+cp_2comp <- function(params, time, dose, iv.dose)
+{
+  if (any(sapply(params,function(x) identical(x,numeric(0))))) return(NA)
+  
+  if (is.null(params$Fgutabs)|is.na(params$Fgutabs)) 
+  {
+    params$Fgutabs<-1
+  }
+  if (is.null(params$kgutabs)|is.na(params$kgutabs)) 
+  {
+    params$kgutabs<-1
+  }
+  if (is.null(params$Fbetaofalpha)) params$Fbetaofalpha<-1
+  if (is.null(params$Ralphatokelim)) params$Ralphatokelim<-1
+  
+  if (is.na(params$Fbetaofalpha)) params$Fbetaofalpha<-1
+  if (is.na(params$Ralphatokelim)) params$Ralphatokelim<-1
+  
+  if (params$Fgutabs>1) params$Fgutabs<-1
+  if (params$Fbetaofalpha>1) params$Fbetaofalpha<-1
+  if (params$Ralphatokelim<0) params$Ralphatokelim<-1
+  
+  alpha <- params$Ralphatokelim*(params$kelim+10^-6)
+  beta <- params$Fbetaofalpha*alpha
+  
+  # try to keep k21 and k12 positive:
+  k21 <- max(min(alpha*beta/params$kelim,alpha+beta-params$kelim),0)
+  k12 <- alpha + beta - params$kelim - k21
+  
+  alphabeta.sum <- alpha + beta
+  alphabeta.prod <- alpha*beta
+  
+  if (iv.dose){ #for IV dosing
+    A <- (dose*(alpha - k21))/(params$V1*(alpha-beta))
+    B <- (dose*(k21-beta))/(params$V1*(alpha-beta))
+    
+    cp <- A*exp(-alpha*time) + B*exp(-beta*time)
+  }else{ #for oral dosing
+    A <- (params$Fgutabs*dose*(alpha - k21))/(params$V1*(alpha-beta))
+    B <- (params$Fgutabs*dose*(k21-beta))/(params$V1*(alpha-beta))
+    C <- -(A+B)
+    cp <-  A*exp(-alpha*time) + B*exp(-beta*time) + C*exp(-params$kgutabs*time)
+  }
+  
+  cp[cp < 10^-20] <- 10^-20
+  return(cp)
+}
 
 # This function does the level II concentration comparisons:
-makeCvTpredsfromfits <- function(CvT.data,label)
+makeCvTpredsfromfits <- function(
+  CvT.data, # Concentration vs. time data
+  onecompfits, # One compartment model fits
+  twocompfits = NULL, # Two compartment model fits (if available)
+  label) # How the predictions should be labeled
 {
   cvt.table <- NULL
   stats.table <- NULL
   for (this.cas in unique(CvT.data$CAS))
   {
-    if (this.cas %in% cvtfits$CAS & 
-      this.cas %in% get_cheminfo(model="1compartment"))
+    onecomp.AIC <- NA
+    twocomp.AIC <- NA
+    if (this.cas %in% onecompfits$CAS)
     {
-      this.fit <- subset(cvtfits, CAS==this.cas)
+      this.fit1 <- subset(onecompfits, 
+        CAS==this.cas & 
+        param.value.type=="Fitted geometric mean")
+      if (dim(this.fit1)[1]>1)
+      {
+        if (length(unique(tolower(this.fit1$Species)))==1)
+        {
+          this.fit1 <- subset(this.fit1, Data.Analyzed=="Joint Analysis")
+        } else {
+          this.fit1.new <- NULL
+          for (this.species in unique(tolower(this.fit1$Species)))
+          {
+            this.fit1.speciessub <- subset(this.fit1, 
+                                      tolower(Species) == this.species)
+            if (dim(this.fit1.speciessub)[1]>1)
+            {
+              this.fit1.new <- rbind(this.fit1.new, 
+                subset(this.fit1.speciessub, Data.Analyzed=="Joint Analysis"))
+            } else {
+              this.fit1.new <- rbind(this.fit1.new, this.fit1.speciessub)
+            }
+          }
+          this.fit1 <- this.fit1.new
+        }
+      }
+      if (dim(this.fit1)[1]>0) onecomp.AIC <- this.fit1$AIC
+    }
+    if (!is.null(twocompfits))
+    {
+      if (this.cas %in% twocompfits$CAS)
+      {
+        this.fit2 <- subset(twocompfits, 
+                           CAS==this.cas & 
+                             param.value.type=="Fitted geometric mean")
+        if (dim(this.fit2)[1]>1)
+        {
+          if (length(unique(tolower(this.fit2$Species)))==1)
+          {
+            this.fit2 <- subset(this.fit2, Data.Analyzed=="Joint Analysis")
+          } else {
+            this.fit2.new <- NULL
+            for (this.species in unique(tolower(this.fit2$Species)))
+            {
+              this.fit2.speciessub <- subset(this.fit2, 
+                                             tolower(Species) == this.species)
+              if (dim(this.fit2.speciessub)[1]>1)
+              {
+                this.fit2.new <- rbind(this.fit2.new, 
+                                       subset(this.fit2.speciessub, 
+                                              Data.Analyzed=="Joint Analysis"))
+              } else {
+                this.fit2.new <- rbind(this.fit2.new, this.fit2.speciessub)
+              }
+            }
+            this.fit2 <- this.fit2.new
+          }
+        }
+        if (dim(this.fit2)[1]>0) twocomp.AIC <- this.fit2$AIC
+      }
+    }
+    
+    if (is.na(twocomp.AIC))
+    {
+      # If there is no two compartment fit:
+      if (is.na(onecomp.AIC)) model <- NULL
+      else model <- "cp_1comp"
+    } else if (is.na(onecomp.AIC))
+    {
+      # If there is a two comparment fit but no one compartment fit:
+      model <- "cp_2comp"
+    } else if (length(onecomp.AIC) > length(twocomp.AIC))
+    {
+      # If the one compartment model worked for more species:
+      model <- "cp_1comp"
+    } else if (length(onecomp.AIC) < length(twocomp.AIC))
+    {
+      # If the two compartment model worked for more species:
+      model <- "cp_2comp"
+    } else if (onecomp.AIC < twocomp.AIC)
+    {
+      # Basic AIC test, two models, same data, lower value wins:
+      model <- "cp_1comp"
+    } else model <- "cp_2comp"
+    
+    if (model == "cp_1comp") this.fit <- this.fit1
+    else if (model == "cp_2comp") this.fit <- this.fit2
+    
+    if (!is.null(model))
+    {
       this.subset1 <- subset(CvT.data,CAS==this.cas & 
         !is.na(Time) & 
         !is.na(Value))
       this.compound <- this.subset1$Compound[1]
       this.dtxsid <- this.subset1$DTXSID[1]
-      for (this.species in unique(this.subset1$Species))
+      for (this.species in unique(tolower(this.subset1$Species)))
         if (tolower(this.species) %in% tolower(this.fit$Species))
         {
-          params <- suppressWarnings(parameterize_1comp(
-            chem.cas=this.cas,
-            species=this.species,
-            default.to.human=TRUE,
-            suppress.messages=TRUE))
+          params <- NULL
           fit.index <- tolower(this.fit$Species)==tolower(this.species)
-          params$Vdist <- as.numeric(this.fit[fit.index,"Vdist"])
-          params$kelim <- as.numeric(this.fit[fit.index,"kelim"]) 
-          if (!is.na(this.fit[fit.index,"Fgutabs"])) 
+          if (model == "cp_1comp")
           {
-            params$Fgutabs <- as.numeric(this.fit[1,"Fgutabs"])
+            params$Vdist <- as.numeric(this.fit[fit.index,"Vdist"])
+            params$kelim <- as.numeric(this.fit[fit.index,"kelim"]) 
+          } else {
+            params$V1 <- as.numeric(this.fit[fit.index,"V1"])
+            params$kelim <- as.numeric(this.fit[fit.index,"kelim"]) 
+            params$Ralphatokelim <- as.numeric(this.fit[fit.index,"Ralphatokelim"])
+            params$Fbetaofalpha <- as.numeric(this.fit[fit.index,"Fbetaofalpha"])
           }
-          if (!is.na(this.fit[fit.index,"kgutabs"])) 
-          {
-            params$kgutabs<- as.numeric(this.fit[1,"kgutabs"])
-          }
-          this.subset2 <- subset(this.subset1,Species==this.species)
+          
+          params$Fgutabs <- as.numeric(this.fit[1,"Fgutabs"])
+          params$kgutabs<- as.numeric(this.fit[1,"kgutabs"])
+
+          this.subset2 <- subset(this.subset1,tolower(Species)==this.species)
           for (this.route in unique(this.subset2$Route))
           {
             this.subset3 <- subset(this.subset2,Route==this.route)
@@ -145,22 +365,15 @@ makeCvTpredsfromfits <- function(CvT.data,label)
             {
               this.subset4 <- subset(this.subset3,Dose==this.dose)
               obs.times <- signif(sort(unique(this.subset4$Time)),4)
-              pred <- suppressWarnings(solve_1comp(
-                parameters=params,
-                times=sort(unique(c(seq(0,2,0.05),obs.times))),
-                iv.dose=(this.route=="iv"),
-                dose=this.dose,
-                suppress.messages=TRUE))
-              pred <- subset(pred,pred[,"time"]>0.0001)
-              # Convert from uM to ug/mL:
-              pred[,"Ccompartment"] <- pred[,"Ccompartment"]*params$MW/1000
-              if (any(tolower(unlist(this.subset4[,"Media"]))=="blood"))
-              {
-                Rb2p <- suppressWarnings(available_rblood2plasma(
-                  chem.cas=this.cas,
-                  species=this.species,
-                  suppress.messages=TRUE))
-              }
+              # Dose is in mg/kg. Vd is in L/kg
+              pred <- suppressWarnings(do.call(
+                model,
+                list(
+                  time = obs.times,
+                  params = params,
+                  dose = this.dose,
+                  iv.dose = tolower(this.route)=="iv")))
+              # I think output is already in ug/mL (mg/L)
               this.subset4means <- NULL
               for (this.time in obs.times)
               {
@@ -173,17 +386,12 @@ makeCvTpredsfromfits <- function(CvT.data,label)
                   Route=this.route,
                   Dose=this.dose,
                   Time=this.time,
-                  Conc.pred=pred[pred[,"time"] == this.time,"Ccompartment"],
+                  Conc.pred=pred[obs.times == this.time],
                   stringsAsFactors=FALSE)
                 new.row <- merge(new.row,
                   this.subset5[,c(
                     "CAS","Media","Value","Source","calc_loq")], by="CAS")
                 colnames(new.row)[colnames(new.row)=="Value"] <- "Conc.obs"
-                if (any(tolower(new.row[,"Media"])=="blood"))
-                {
-                  new.row[tolower(new.row[,"Media"])=="blood","Conc.pred"] <-
-                    Rb2p * new.row[tolower(new.row[,"Media"])=="blood","Conc.pred"]
-                }
                 cvt.table <- rbind(cvt.table,new.row)
                 this.subset4means <- rbind(this.subset4means,
                   data.frame(
@@ -191,9 +399,15 @@ makeCvTpredsfromfits <- function(CvT.data,label)
                     Value=mean(this.subset5$Value,na.rm=0)))
               }
               Cmax.obs <- max(this.subset4means$Value,na.rm=T)
-              Cmax.pred <- max(pred[,"Ccompartment"],na.rm=T)
-              AUC.obs <- AUC(this.subset4means$Time,this.subset4means$Value)
-              AUC.pred <- AUC(pred[,"time"],pred[,"Ccompartment"])
+              Cmax.pred <- max(pred,na.rm=T)
+              if (length(obs.times)>1)
+              {
+                AUC.obs <- AUC(this.subset4means$Time,this.subset4means$Value)
+                AUC.pred <- AUC(obs.times,pred)
+              } else {
+                AUC.obs <- NA
+                AUC.pred <- NA
+              }
               new.row <- data.frame(
                 Compound=this.compound,
                 DTXSID=this.dtxsid,
@@ -220,7 +434,10 @@ makeCvTpredsfromfits <- function(CvT.data,label)
 }
 
 # This function does the level III statistic comparisons:
-maketkstatpreds <- function(CvT.data,label)
+maketkstatpreds <- function(
+  CvT.data,
+  cvtfits,
+  label)
 {
   out.table <- NULL
   for (this.cas in unique(cvtfits$CAS))
